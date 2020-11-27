afeaa15cfcd54f19132ce6e113af4482aed2c0ce
camacho1607 <ach-1000@hotmail.com>
2020-11-26 18:23:47 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,9 +11,9 @@ public class Main
     {
         Scanner scan = new Scanner(System.in);
         Lector lector= new Lector("Archivos/Ecuaciones.txt");
-        SepararEcuaciones separarEcuaciones= new SepararEcuaciones(lector.leerLinea());
-        Separador separarEcuacion1= new Separador(separarEcuaciones.getEcuacion1());
-        Separador separarEcuacion2= new Separador(separarEcuaciones.getEcuacion2());
+        SeparadorEcuaciones separarEcuaciones= new SeparadorEcuaciones(lector.leerLinea());
+        SeparadorMonomios separarEcuacion1= new SeparadorMonomios(separarEcuaciones.getEcuacion1());
+        SeparadorMonomios separarEcuacion2= new SeparadorMonomios(separarEcuaciones.getEcuacion2());
         System.out.println(separarEcuaciones.getEcuacion1());
         
         System.out.println("Que operacion desea hacer?");
@@ -25,13 +25,14 @@ public class Main
             if( op != 4) {
             Ecuacion ecuacion1 = new Ecuacion();
             Ecuacion ecuacion2 = new Ecuacion();
-            for(int potencia=0;potencia<separarEcuacion1.getGrado()+1;potencia++)
+            for(int indice=0;indice<separarEcuacion1.getCantidadMonomios()+1;indice++)
             {
-                ecuacion1.AgregarAModelo(separarEcuacion1.coeficientes[potencia],separarEcuacion1.getIncognita(),potencia);
-                ecuacion2.AgregarAModelo(separarEcuacion2.coeficientes[potencia],separarEcuacion1.getIncognita(),potencia);
-
+                ecuacion1.AgregarAModelo(separarEcuacion1.getCoeficiente(indice),separarEcuacion1.getIncognita(),separarEcuacion1.getPotencia(indice));
             }
-            
+            for(int indice=0;indice<separarEcuacion2.getCantidadMonomios()+1;indice++)
+            {
+                ecuacion2.AgregarAModelo(separarEcuacion2.getCoeficiente(indice),separarEcuacion2.getIncognita(),separarEcuacion2.getPotencia(indice));
+            }
             ecuacion1.imprimirEcuacion();
             Operacion Operacion = new Operacion(ecuacion1, ecuacion2);
             Operacion.SwitchOperacion(op);
